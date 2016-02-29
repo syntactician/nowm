32b15b022ceaa6c878c9f0148d3c1a96f3e15c66
Edward Hernandez <ehernandez@email.wm.edu>
2016-02-28 22:16:23 -0500

    now writing to /tmp/ , must be called with wid argument

--- full.sh
+++ full.sh
@@ -3,10 +3,13 @@
 # e.hernandez - 2016
 # first attempt at implementing full-screen windows
 
+ARGS=$@
+
 PANEL=15
 GAP=25
 
-WID=$(pfw)
+FSFILE=/tmp/nowm/full
+
 BW=$(wattr b $WID)
 
 ROOT=$(lsw -r)
@@ -15,17 +18,38 @@ SH=$(wattr h $ROOT)
 
 SH=$((SH - PANEL))
 
-WW=$(($SW - 2*$GAP - 2*$BW))
-WH=$(($SH - 2*$GAP - 2*$BW - $PANEL))
+WW=$((SW - 2*GAP - 2*BW))
+WH=$((SH - 2*GAP - 2*BW - PANEL))
 
-#if [ "$wd" != "$rd" ]; then
-if [ -f ~/.wm/.fw ] ; then
-	# 10 points if you can remove this
-	# UUOC w/out breaking the functionality
-	wtp `cat ~/.wm/.fw`
-	rm -f ~/.wm/.fw
-else
-	wattr xywhi $WID  >~/.wm/.fw
-	# wtp -$bw -$bw $rd $WID
-	wtp $GAP $((GAP + PANEL)) $WW $WH $WID
-fi
+usage() {
+	echo "usage: $(basename $0) <wid>" >&2
+	exit 1
+}
+
+full() {
+	echo $1
+	wattr xywhi $1 > $FSFILE
+	wtp $GAP $((GAP + PANEL)) $WW $WH $1
+}
+
+restore() {
+	# 10 points if you rm cat w/out breaking
+	wtp `cat $FSFILE`
+	rm -f $FSFILE
+}
+
+main () {
+	if [ -f $FSFILE ] ; then
+		FSID=$(awk '{print $NF}' $FSFILE)
+		if [ $FSID = $1 ] ; then
+			restore
+		else
+			restore
+			full $1
+		fi
+	else
+		full $1
+	fi
+}
+
+main $ARGS
